5b34f612c44e5d65ae36551a1ef21deb5da3c3c6
Luke Walker4 <lwalker128005@gmail.com>
2018-04-05 10:52:22 -0600

    Added Technician form

---
+++ MaintainProducts/Technician.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace MaintainProducts
+{
+    public partial class Technician : Form
+    {
+        public Technician()
+        {
+            InitializeComponent();
+        }
+
+        private void techniciansBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+
+            try
+            {
+                this.techniciansBindingSource.EndEdit();
+                this.tableAdapterManager.UpdateAll(this.techSupportDataSet);
+            }
+            catch(DBConcurrencyException ex)
+            {
+                MessageBox.Show("A concurrency error occured");
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Database error # " + ex.Number + ": " + ex.Message, ex.GetType().ToString());
+            }
+
+        }
+
+        private void Technician_Load(object sender, EventArgs e)
+        {
+            try
+            { // TODO: This line of code loads data into the 'techSupportDataSet.Technicians' table. You can move, or remove it, as needed.
+                this.techniciansTableAdapter.Fill(this.techSupportDataSet.Technicians);
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Database error # " + ex.Number + ": " + ex.Message, ex.GetType().ToString());
+            }
+        }
+    }
+}
